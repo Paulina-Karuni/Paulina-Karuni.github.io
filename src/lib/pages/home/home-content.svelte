2ad694c7d796bc115ba8ceedb5f7f46fa71f7f62
joannaflutterapps <161826739+joannaflutterapps@users.noreply.github.com>
2025-09-18 10:21:04 +0200

    Initial commit

---
+++ src/lib/pages/home/home-content.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<h1 class="font-bold">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn btn-ghost"> Hello </button>
+<p>{m.example_message({ username: 'dash' })}</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import HomeContent from '$lib/pages/home/home-content.svelte';
+</script>
+
+<HomeContent />
